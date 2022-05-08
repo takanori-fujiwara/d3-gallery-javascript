9a92128ab7685761133252f8432f21f1f058683c
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2022-05-08 21:03:18 +0200

    added stacked normalized bar chart

---
+++ stacked-normalized-horizontal-bar/script/main.js
@@ -0,0 +1,34 @@
+import {
+  stackedBarChart
+} from './chart.js';
+
+import {
+  legend
+} from './legend.js';
+
+const states = await d3.csv('../data/us-population-state-age.csv');
+const ages = states.columns.slice(1);
+const stateages = ages.flatMap(age => states.map(d => ({
+  state: d.name,
+  age,
+  population: d[age]
+}))); // pivot longer
+
+
+const chart = stackedBarChart(stateages, {
+  x: d => d.population,
+  y: d => d.state,
+  z: d => d.age,
+  yDomain: d3.groupSort(
+    stateages,
+    (D) => D[0].population / d3.sum(D, d => d.population), // proportion of first age group
+    d => d.state // sort y by x
+  ),
+  zDomain: ages,
+  colors: d3.schemeSpectral[ages.length],
+  height: 800
+});
+
+legend(chart.scales.color, {
+  title: 'Age (years)'
+})
