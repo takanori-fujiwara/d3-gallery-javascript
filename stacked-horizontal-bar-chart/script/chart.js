28ac4cecf4060e801a4a5b1c4809c2759cd477b9
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2022-05-08 20:58:25 +0200

    implemented stacked horizontal bar charts

---
+++ stacked-horizontal-bar-chart/script/chart.js
@@ -0,0 +1,140 @@
+/// Moridied source copyright
+// Copyright 2022 Takanori Fujiwara.
+// Released under the BSD 3-Clause 'New' or 'Revised' License
+
+/// Original source copyright
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/stacked-horizontal-bar-chart
+
+export const stackedBarChart = (data, {
+  x = d => d, // given d in data, returns the (quantitative) x-value
+  y = (d, i) => i, // given d in data, returns the (ordinal) y-value
+  z = () => 1, // given d in data, returns the (categorical) z-value
+  title, // given d in data, returns the title text
+  marginTop = 30, // top margin, in pixels
+  marginRight = 0, // right margin, in pixels
+  marginBottom = 0, // bottom margin, in pixels
+  marginLeft = 40, // left margin, in pixels
+  width = 640, // outer width, in pixels
+  height, // outer height, in pixels
+  xType = d3.scaleLinear, // type of x-scale
+  xDomain, // [xmin, xmax]
+  xRange = [marginLeft, width - marginRight], // [left, right]
+  yDomain, // array of y-values
+  yRange, // [bottom, top]
+  yPadding = 0.1, // amount of y-range to reserve to separate bars
+  zDomain, // array of z-values
+  offset = d3.stackOffsetDiverging, // stack offset method
+  order = d3.stackOrderNone, // stack order method
+  xFormat, // a format specifier string for the x-axis
+  xLabel, // a label for the x-axis
+  colors = d3.schemeTableau10, // array of colors
+} = {}) => {
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  const Z = d3.map(data, z);
+
+  // Compute default y- and z-domains, and unique them.
+  if (yDomain === undefined) yDomain = Y;
+  if (zDomain === undefined) zDomain = Z;
+  yDomain = new d3.InternSet(yDomain);
+  zDomain = new d3.InternSet(zDomain);
+
+  // Omit any data not present in the y- and z-domains.
+  const I = d3.range(X.length).filter(i => yDomain.has(Y[i]) && zDomain.has(Z[i]));
+
+  // If the height is not specified, derive it from the y-domain.
+  if (height === undefined) height = yDomain.size * 25 + marginTop + marginBottom;
+  if (yRange === undefined) yRange = [height - marginBottom, marginTop];
+
+  // Compute a nested array of series where each series is [[x1, x2], [x1, x2],
+  // [x1, x2], …] representing the x-extent of each stacked rect. In addition,
+  // each tuple has an i (index) property so that we can refer back to the
+  // original data point (data[i]). This code assumes that there is only one
+  // data point for a given unique y- and z-value.
+  const series = d3.stack()
+    .keys(zDomain)
+    .value(([, I], z) => X[I.get(z)])
+    .order(order)
+    .offset(offset)
+    (d3.rollup(I, ([i]) => i, i => Y[i], i => Z[i]))
+    .map(s => s.map(d => Object.assign(d, {
+      i: d.data[1].get(s.key)
+    })));
+
+  // Compute the default x-domain. Note: diverging stacks can be negative.
+  if (xDomain === undefined) xDomain = d3.extent(series.flat(2));
+
+  // Construct scales, axes, and formats.
+  const xScale = xType(xDomain, xRange);
+  const yScale = d3.scaleBand(yDomain, yRange).paddingInner(yPadding);
+  const color = d3.scaleOrdinal(zDomain, colors);
+  const xAxis = d3.axisTop(xScale).ticks(width / 80, xFormat);
+  const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
+
+  // Compute titles.
+  if (title === undefined) {
+    const formatValue = xScale.tickFormat(100, xFormat);
+    title = i => `${Y[i]}\n${Z[i]}\n${formatValue(X[i])}`;
+  } else {
+    const O = d3.map(data, d => d);
+    const T = title;
+    title = i => T(O[i], i, data);
+  }
+
+  d3.select('body').select('svg').remove();
+
+  const svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewBox', [0, 0, width, height])
+    .attr('style', 'max-width: 100%; height: auto; height: intrinsic;');
+
+  svg.append('g')
+    .attr('transform', `translate(0,${marginTop})`)
+    .call(xAxis)
+    .call(g => g.select('.domain').remove())
+    .call(g => g.selectAll('.tick line').clone()
+      .attr('y2', height - marginTop - marginBottom)
+      .attr('stroke-opacity', 0.1))
+    .call(g => g.append('text')
+      .attr('x', width - marginRight)
+      .attr('y', -22)
+      .attr('fill', 'currentColor')
+      .attr('text-anchor', 'end')
+      .text(xLabel));
+
+  const bar = svg.append('g')
+    .selectAll('g')
+    .data(series)
+    .join('g')
+    .attr('fill', ([{
+      i
+    }]) => color(Z[i]))
+    .selectAll('rect')
+    .data(d => d)
+    .join('rect')
+    .attr('x', ([x1, x2]) => Math.min(xScale(x1), xScale(x2)))
+    .attr('y', ({
+      i
+    }) => yScale(Y[i]))
+    .attr('width', ([x1, x2]) => Math.abs(xScale(x1) - xScale(x2)))
+    .attr('height', yScale.bandwidth());
+
+  if (title) bar.append('title')
+    .text(({
+      i
+    }) => title(i));
+
+  svg.append('g')
+    .attr('transform', `translate(${xScale(0)},0)`)
+    .call(yAxis);
+
+  return Object.assign(svg.node(), {
+    scales: {
+      color
+    }
+  });
+}
--- stacked-horizontal-bar-chart/script/main.js
+++ stacked-horizontal-bar-chart/script/main.js
@@ -6,7 +6,6 @@ import {
   legend
 } from './legend.js';
 
-
 const states = await d3.csv('../data/us-population-state-age.csv');
 const ages = states.columns.slice(1);
 const stateages = ages.flatMap(age => states.map(d => ({
@@ -15,16 +14,16 @@ const stateages = ages.flatMap(age => states.map(d => ({
   population: d[age]
 }))); // pivot longer
 
+
 const chart = stackedBarChart(stateages, {
-  x: d => d.state,
-  y: d => d.population / 1e6,
+  x: d => d.population / 1e6,
+  y: d => d.state,
   z: d => d.age,
-  xDomain: d3.groupSort(stateages, D => d3.sum(D, d => -d.population), d => d.state),
-  yLabel: "↑ Population (millions)",
+  xLabel: 'Population (millions) →',
+  yDomain: d3.groupSort(stateages, D => d3.sum(D, d => d.population), d => d.state), // sort y by x
   zDomain: ages,
   colors: d3.schemeSpectral[ages.length],
-  width: 1000,
-  height: 500
+  height: 800
 });
 
 legend(chart.scales.color, {
