03ce7b9521dc42516109995a9f30b32d792d11e4
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2022-05-18 23:06:54 +0200

    radial examples

---
+++ radial/donut-chart/script/main.js
@@ -0,0 +1,15 @@
+// Copyright 2022 Takanori Fujiwara.
+// Released under the BSD 3-Clause 'New' or 'Revised' License
+
+import {
+  donutChart
+} from './chart.js';
+
+const population = await d3.csv('./data/population-by-age.csv', d3.autoType);
+
+donutChart(population, {
+  name: d => d.name,
+  value: d => d.value,
+  width: 1000,
+  height: 500
+});
