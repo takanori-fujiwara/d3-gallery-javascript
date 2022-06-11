7aff023483b1c9a90e73082cec7835d2b59d4bd6
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2022-06-11 19:46:08 +0200

    modified copyright

--- just-for-fun/logo/script/chart.js
+++ just-for-fun/logo/script/chart.js
@@ -3,9 +3,21 @@
 // Released under the BSD 3-Clause 'New' or 'Revised' License
 
 /// Original source copyright
-// Copyright 2021 Observable, Inc.
+// Copyright 2014–2020 Mike Bostock
 // Released under the ISC license.
 // https://observablehq.com/@d3/logo
+// > ISC License
+// > Permission to use, copy, modify, and/or distribute this software for any
+// > purpose with or without fee is hereby granted, provided that the above
+// > copyright notice and this permission notice appear in all copies.>
+//
+// > THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// > WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// > MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// > ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// > WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// > ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// > OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 export const logo = ({
   svgId = 'logo',
